51afc4d14fc826fe5beec8f2ce608d9462232269
Nirmal Thomas Mathew <nirmal13mathew@gmail.com>
2025-01-04 14:28:37 +0530

    added fastapi server

---
+++ app.py
@@ -0,0 +1,13 @@
+# app.py
+from fastapi import FastAPI
+from fastapi_socketio import SocketManager
+
+app = FastAPI()
+socket_manager = SocketManager(app=app)
+
+
+@app.sio.on('join')
+async def handle_join(sid, *args, **kwargs):
+    await app.sio.emit("lobby", 'User joined')
+
+
